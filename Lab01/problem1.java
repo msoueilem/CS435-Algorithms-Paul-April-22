e1a52d2991102a2df28cdc0095a26aa6b66a48fe
Soueilem Mohamed Lemine <msoueilem@miu.edu>
2022-04-25 15:39:13 -0500

    Problem1

--- Lab01/problem1.java
+++ Lab01/problem1.java
@@ -1,24 +1,27 @@
-class Problem1{
+class Problem1 {
+
     public static boolean sumFound(int[] arr, int z) {
-        if(arr==null || arr.length < 2) {
-        return false;
+        if (arr == null || arr.length < 2) {
+            return false;
         }
-        //implement
+        // implement
         for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < arr.length; j++) {
-                if(i==j){
+                if (i == j) {
                     continue;
                 }
-                if(arr[i]+arr[j]==z){
-                    System.out.printf("Index i: %d data of Index %d Index j: %d data of Index %d \n%d+%d=%d\n",i,arr[i],j,arr[j],arr[i],arr[j],z);
+                if (arr[i] + arr[j] == z && arr[i] != arr[j]) {
+                    System.out.printf("Index i: %d data of Index %d Index j: %d data of Index %d \n%d+%d=%d\n", i,
+                            arr[i], j, arr[j], arr[i], arr[j], z);
                     return true;
                 }
             }
         }
         return false;
     }
+
     public static void main(String[] args) {
-        int[] a = {1,2,3,4,5,6,7,8};
-        System.out.println(sumFound(a, 2));
+        int[] b = { 3, 3, 4, 7 };
+        System.out.println(sumFound(b, 6));
     }
 }
