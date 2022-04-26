867091f4e4941dea59c35c3b684e788e7a0a8d51
Soueilem Mohamed Lemine <msoueilem@miu.edu>
2022-04-26 17:11:42 -0500

    Day 2 package

--- src/Assignment1A/Problem1.java
+++ src/Assignment1A/Problem1.java
@@ -14,7 +14,7 @@ class Problem1 {
         }
         // implement
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
+            for (int j = 0; j < i; j++) {
                 if (i == j) {
                     continue;
                 }
--- src/InClass/Day2.java
+++ src/InClass/Day2.java
@@ -1,2 +1,7 @@
-package InClass;public class Day2 {
+package InClass;
+
+public class Day2 {
+    public static void main(String[] args) {
+        System.out.println("From Day2");
+    }
 }
