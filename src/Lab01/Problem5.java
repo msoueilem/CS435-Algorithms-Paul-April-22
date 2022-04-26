ecf8eaf3b05033479e46b74ef1bcd0c6662057d2
Soueilem Mohamed Lemine <msoueilem@miu.edu>
2022-04-26 01:34:35 -0500

    Problem4 part 1 done. Problem5 part 1 done.

---
+++ src/Lab01/Problem5.java
@@ -0,0 +1,37 @@
+/**
+ * (A) Determine whether z is in arr.
+ * (B) Suppose the array arr is already known to be in sorted order. Can you write a more efficient
+ * program for determining whether z is in arr? If yes, why do you think your solution to (B)
+ * is faster?
+ */
+package Lab01;
+
+public class Problem5 {
+    public static boolean find(int[] arr, int z) {
+        //implement
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]==z){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static boolean findInSorted(int[] arr, int z) {
+        //implement
+        if (arr[0]<z||arr[arr.length]>z){
+            return false;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]==z){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] b = { 3, 3, 4, 7 };
+        int[] a = { 2, 3, 4, 7 };
+        System.out.println(find(b,4));
+        System.out.println(findInSorted(a,9));
+    }
+}
